dcd2ce53ad9dda5b60159ba8df39b8f913a83552
Gulzarokz <gokz22301@gamil.com>
2024-10-27 20:01:13 +0500

    Done string-replace-character & rounding numbers

---
+++ index.js
@@ -0,0 +1,131 @@
+//25 Strings: Replacing characters
+
+// const fruit = "Apple";
+// const modifiedFruit = fruit.replace('Apple', "Banana");
+// console.log(fruit);
+// console.log(modifiedFruit);
+
+// const fruit = "Apple";
+// const modifiedFruit = fruit.replace('Apple', "apple");
+// console.log(fruit);
+// console.log(modifiedFruit);
+
+// const newText = "This is your text";
+// const modifiedText = newText.replace('your', 'our');
+// console.log(newText);
+// console.log(modifiedText);
+
+// const newText = "This is your text";
+// const modifiedText = newText.replace('your', ' ');
+// console.log(newText);
+// console.log(modifiedText);
+
+// const newText = "This is your text";
+// const modifiedText = newText.replace('text', ' text ?');
+// console.log(newText);
+// console.log(modifiedText);
+
+
+// it modifiedText on the first match
+// const text = "shut up? I'm own boss! shut up!";
+// const modifiedText = text.replace('shut up', '...');
+// console.log(text);
+// console.log(modifiedText);
+
+
+// // it match globally 
+
+// const text = "shut up I'm own boss! shut up!";
+// const modifiedText = text.replace(/shut up/g, '...');
+// console.log(text);
+// console.log(modifiedText);
+
+// var x = "abc";
+// var y = x.replace("a", "z");
+// console.log(x);
+
+// console.log(y);
+
+//26  Rounding numbers
+
+
+// Methods we are ganna to study
+
+// 1 round() -> it round the number to nearest integers
+// 2 ceil() -> it is round the number up to the nearest integers
+// 3 floor() -> it is round the number down to the nesrest intergers
+
+// const percentage = 73.56;
+// console.log(Math.round(percentage));
+
+// const a = 2.6;
+// console.log(Math.round(a));
+
+// const b = 2.5;
+// console.log(Math.round(b));
+
+// const c = 2.4;
+// console.log(c);
+
+// const a = Math.round(2.6);
+// console.log(a);
+
+// const b = Math.round(2.5);
+// console.log(b);
+
+// const c = Math.round(2.49);
+// console.log(c);
+
+// Negative value are consider to be dwon values such is -1 , -2, -3, etc;
+
+// const a = Math.round(-2.6);
+// console.log(a);
+
+// const b = Math.round(-2.5);
+// console.log(b);
+
+// const c = Math.round(-2.49);
+// console.log(c);
+
+// Ceil Methods
+
+// const a = Math.ceil(1.4)
+// console.log(a);
+
+// const b = Math.ceil(0.6)
+// console.log(b);
+
+
+// const c = Math.ceil(0.4)
+// console.log(c);
+
+// const d = Math.ceil(5.1)
+// console.log(d);
+
+// const e = Math.ceil(-5.6)
+// console.log(e);
+
+// const f = Math.ceil(-5.4)
+// console.log(f);
+
+// floor Methods
+
+// const a = Math.floor(1.4)
+// console.log(a);
+
+// const b = Math.floor(0.6)
+// console.log(b);
+
+
+// const c = Math.floor(0.4)
+// console.log(c);
+
+// const d = Math.floor(5.1)
+// console.log(d);
+
+// const e = Math.floor(-5.6)
+// console.log(e);
+
+// const f = Math.floor(-5.4)
+// console.log(f);
+
